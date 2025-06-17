c035874e9cbd6e2c5c99197f6113e1ba0b7ac238
floridaalan <128455442+floridaalan@users.noreply.github.com>
2025-06-17 22:16:17 +0530

    Create isPalindromeLL.java

---
+++ isPalindromeLL.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+     private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode newHead=reverse(slow.next);
+        ListNode first=head;
+        ListNode second=newHead;
+        while(second!=null){
+            if(first.val!=second.val){
+                reverse(newHead);
+                return false;
+            }
+            first=first.next;
+            second=second.next;
+        }
+        return true;
+    }
+        
+       
+}
